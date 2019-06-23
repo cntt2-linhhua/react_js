55bae82150c48867bffc6cfd8e566995cbafa093
linh.huathi <linhhuacntt@gmail.com>
2019-06-23 01:26:52 -0700

    [PHAN-02] 006 Dinh nghia component theo cach so 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function One() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <h1>So 1 tap 1</h1>
+    <h5>So 1 tap cuoi</h5>
+  </div> );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />	        
+        <One/>	
+        <hr/>
+        </header>	      
+    </div>	    
+    );
+  }
 }
 
 export default App;
+
