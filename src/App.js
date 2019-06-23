dc31e6b5cf8911317c51081d3e469ff3e63f48ca
linh.huathi <linhhuacntt@gmail.com>
2019-06-23 01:40:21 -0700

    [PHAN-02] 007 Dinh nghia component bang anonymous function va arrow Function

--- src/App.js
+++ src/App.js
@@ -10,15 +10,42 @@ function One() {
     <h5>So 1 tap cuoi</h5>
   </div> );
 }
+var Two = function()
+{
+  return (
+    <div>
+    <h3> Cach so 2 </h3>
+    </div>
+  );
+}
 
+ var Three = () =>(
+  <div>
+     <h2> so 1 tap 1 </h2>	    
+     <h3> so 1 tap 2 </h3>	    
+     </div>	
+     );
+
+ class Four extends Component {
+  render() {
+    return (
+      <div>
+         <button> React JS component so 4 </button>
+      </div>
+    );
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />	        
-        <One/>	
+        <One/>
+        <Two/>
+        <Three/>
         <hr/>
+        <Four/>	
         </header>	      
     </div>	    
     );
